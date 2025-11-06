5a02e5976a53275033460a8a763a5ac5514ac6e1
Abedi <Abedi@pulseware.com>
2025-11-06 13:49:31 +0330

    Products models added.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Category(models.Model):
+    parent=models.ForeignKey('self',verbose_name=_('parent'), on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'),blank=True)
+    avatar=models.ImageField(_('avatar'),upload_to="categories/",blank=True)
+    is_enabled = models.BooleanField(_('enabled'),default=True)
+    created_time = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'),auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+class Product(models.Model):
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'),blank=True)
+    avatar=models.ImageField(_('avatar'),upload_to="products/",blank=True)
+    is_enabled = models.BooleanField(_('enabled'),default=True)
+    categories=models.ManyToManyField(Category,verbose_name=_('categories'),blank=True)
+    created_time = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'),auto_now=True)
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+
+class File(models.Model):
+    product = models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    file = models.FileField(_('file'),upload_to="files/%Y/%m/%d/")
+    is_enabled = models.BooleanField(_('is enable'),default=True)
+    created_time = models.DateTimeField(_('created time'),auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'),auto_now=True)
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
+
